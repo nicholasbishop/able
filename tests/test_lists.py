b3ae85126b700fc4c18a094e433527317ca2a358
Nicholas Bishop <nicholasbishop@gmail.com>
2017-09-29 17:19:03 -0400

    Add list tests

---
+++ tests/test_lists.py
@@ -0,0 +1,23 @@
+import unittest
+
+import able
+
+def parse(string):
+    return able.parse(string, rule_name='list')
+
+
+class TestLists(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(parse('[]'), [])
+
+    def test_numeric(self):
+        self.assertEqual(parse('[1 2 3]'), [1, 2, 3])
+
+    def test_text(self):
+        self.assertEqual(parse('["a" "b" "c"]'), ['a', 'b', 'c'])
+
+    def test_pair(self):
+        self.assertEqual(parse('[key: "value"]'), [('key', 'value')])
+
+    def test_mixed(self):
+        self.assertEqual(parse('["a" 1 b: 2]'), ['a', 1, ('b', 2)])
