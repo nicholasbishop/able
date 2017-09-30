15bf47367fead79a2383fd7500463fc84f097a9b
Nicholas Bishop <nicholasbishop@gmail.com>
2017-09-29 20:04:43 -0400

    Add grammar to package

--- setup.py
+++ setup.py
@@ -20,11 +20,12 @@ from setuptools import setup
 
 setup(
     name='able',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/nicholasbishop/able',
     author='Nicholas Bishop',
     author_email='nicholasbishop@gmail.com',
     license='Apache 2.0',
     packages=['able'],
     install_requires=['TatSu~=4.2.3'],
+    package_data={'able': ['grammar.ebnf']},
 )
