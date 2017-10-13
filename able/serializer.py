69cfe5139ec6d41c9d40343e0be2ab62eb47ed9d
Nicholas Bishop <nicholasbishop@gmail.com>
2017-10-12 22:18:18 -0400

    Serialization fixes

--- able/serializer.py
+++ able/serializer.py
@@ -1,25 +1,63 @@
 import collections
 
+import six
+
+
+class Block(object):
+    def __init__(self):
+        self._output = ''
+        self._indent_level = 0
+
+    def open_value(self, text):
+        pass
+
+    def close_value(self, text):
+        pass
+
+    def simple_value(self, text):
+        pass
+
+
 class Serializer(object):
     def __init__(self):
         self._indent_level = 0
         self._output = ''
+        self._needs_indent = False
 
     def put(self, line):
-        self._output += '  ' * self._indent_level + line + '\n'
+        if self._needs_indent:
+            self._output += '  ' * self._indent_level
+            self._needs_indent = False
+        self._output += line
+
+    def eol(self):
+        self._output += '\n'
+        self._needs_indent = True
 
     def is_valid_pair_key(self, key):
         # TODO
         return isinstance(key, str)
 
-    def serialize_int(self, val):
-        self.put(str(val))
+    def serialize_int(self, data):
+        self.put(str(data))
+        self.eol()
+
+    def serialize_float(self, data):
+        self.put(str(data))
+        self.eol()
+
+    def serialize_string(self, data):
+        self.put("'{}'".format(data))
+        self.eol()
 
     def serialize_pair(self, key, val):
-        self.put('{}: {}'.format(key, val))
+        self.put(key)
+        self.put(': ')
+        self.serialize_any(val)
 
     def serialize_map_inner(self, val):
-        for key in val:
+        # TODO
+        for key in sorted(val):
             # TODO
             if not self.is_valid_pair_key(key):
                 raise KeyError(key)
@@ -27,15 +65,23 @@ class Serializer(object):
 
     def serialize_map(self, val):
         self.put('[')
+        self.eol()
         # TODO "with self.indent..."
         self._indent_level += 1
         self.serialize_map_inner(val)
         self._indent_level -= 1
         self.put(']')
+        self.eol()
 
     def serialize_any(self, data):
         if isinstance(data, collections.Mapping):
             self.serialize_map(data)
+        elif isinstance(data, six.string_types):
+            self.serialize_string(data)
+        elif isinstance(data, int):
+            self.serialize_int(data)
+        elif isinstance(data, float):
+            self.serialize_float(data)
 
     def serialize(self, data):
         # TODO
--- tests/test_write.py
+++ tests/test_write.py
@@ -5,3 +5,14 @@ from able import util
 class TestWrite(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(util.serialize({}), '')
+
+    def test_soften(self):
+        self.assertEqual(
+            util.serialize({'name': 'soften', 'version': '0.1.0'}),
+            "name: 'soften'\nversion: '0.1.0'\n")
+
+    def test_int(self):
+        self.assertEqual(util.serialize({'int': 1}), 'int: 1\n')
+
+    def test_float(self):
+        self.assertEqual(util.serialize({'float': 1.5}), 'float: 1.5\n')
